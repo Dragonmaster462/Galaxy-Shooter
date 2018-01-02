520eb609f898d2ced24f5e266406070bf9eda6bf
Tony Ellicott <dragonmaster462@gmail.com>
2018-01-02 13:05:07 -0500

    added shooting mechanic

--- Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Galaxy Shooter/Scripts/Player.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5.0f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+
+    [SerializeField]
+    private float _fireRate = 0.25f;
+
+    private float _canFire = 0.0f;
+
+    [SerializeField]
+    private float _speed = 5.0f;
 
 	// Use this for initialization
 	void Start ()
@@ -15,11 +24,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Time.time > _canFire)
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.7f, 0), Quaternion.identity);
+            _canFire = Time.time + _fireRate;
+        }
+    }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
-        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * verticalInput * Time.deltaTime);
 
         if (transform.position.y > 0)
         {
