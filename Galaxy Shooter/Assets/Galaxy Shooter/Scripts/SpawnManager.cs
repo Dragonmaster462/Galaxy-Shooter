c46b0793c9a0c358f6bd0d581779510b46c01c3a
Tony Ellicott <dragonmaster462@gmail.com>
2018-01-10 23:41:29 -0500

    added spawn manager

---
+++ Galaxy Shooter/Assets/Galaxy Shooter/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyShipPrefab;
+    [SerializeField]
+    private GameObject[] powerups;
+
+    private float enemySpawnTimer = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(PowerUpSpawnRoutine());
+	}
+	
+    //create coroutine to spawn enemy
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (true)
+        {
+            Instantiate(enemyShipPrefab, new Vector3(Random.Range(-7.97f, 7.97f), 6.07f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator PowerUpSpawnRoutine()
+    {
+        while (true)
+        {
+            int randomPowerup = Random.Range(0, 3);
+
+            Instantiate(powerups[randomPowerup], new Vector3(Random.Range(-7.97f, 7.97f), 6.07f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
